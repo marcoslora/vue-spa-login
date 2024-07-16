ed8c69b5fd8e27399ef562a1eaef74e1905f52e0
Marcos Lora <marcoslora95@gmail.com>
2024-07-16 14:55:51 -0400

    Avances en la web app

---
+++ src/router/index.ts
@@ -0,0 +1,70 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+import NotFound from '@/modules/common/pages/NotFound404.vue';
+
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // Landing
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+        {
+          path: '/pokemon/:id',
+          name: 'pokemon',
+          props: (route) => {
+            const id = Number(route.params.id);
+            return isNaN(id) ? { id: 1 } : { id };
+          },
+          component: () => import('@/modules/pokemons/pages/PokemonPage.vue'),
+        },
+      ],
+    },
+
+    // Auth
+    {
+      path: '/auth',
+      redirect: { name: 'login' }, //'/login',
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
+    },
+
+    // Not Found
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  ],
+});
+
+export default router;
